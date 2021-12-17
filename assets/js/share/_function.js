4afb67484d4281f5195302e01b0c78ec128e4d60
Macbook <macbook@dev-mac.local>
2021-12-17 17:36:33 +0700

    add xmas promotion 17/12/21

--- assets/js/share/_function.js
+++ assets/js/share/_function.js
@@ -62,18 +62,23 @@ export const __init_product_list = (params = { ids: product_ids }) => {
         })"></span></a>
         </div>
         <div class="detail">
-          <h6 class="name">${item.name.toLowerCase()}</h6>
-          <div class="price">
-            ${
-              item.salePrice
-                ? `<p>${__currency_format(item.salePrice)}</p>
-              <p class="discount">${__currency_format(item.price)}</p> `
-                : `<p>${__currency_format(item.price)}</p>`
-            }
+          <div class="info">
+            <h6 class="name">${item.name.toLowerCase()}</h6>
+            <div class="price">
+              ${
+                item.salePrice
+                  ? `<p>${__currency_format(item.salePrice)}</p>
+                <p class="discount">${__currency_format(item.price)}</p> `
+                  : `<p>${__currency_format(item.price)}</p>`
+              }
+            </div>
+            ${item.discount > 0 ? `<p class="tag">${item.discount}%</p>` : ""}
+            <div class="color">
+              <p>+${item.color.length} màu</p>
+            </div>
           </div>
-          ${item.discount > 0 ? `<p class="tag">${item.discount}%</p>` : ""}
-          <div class="color">
-             <p>+${item.color.length} màu</p>
+          <div class="promotion">
+              <p>Giảm thêm tới 300K</p>
           </div>
         </div>
       </div>
